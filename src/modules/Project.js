14feb6f9a8931de7c3a83084c5912625b8b0213c
Matt Ellicott <mattellicott@gmail.com>
2023-11-04 17:36:58 -0500

    add: publicMethods addTask getters: getTitle, getTaskList

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -1,4 +1,20 @@
-export function Project(title) {
+function Project(title) {
   const _title = title;
   const _taskList = {};
+
+  const publicMethods = {
+    addTask: (task) => {
+      _taskList[Object.keys(_taskList).length] = task;
+    },
+
+    getTitle: () => {
+      return _title;
+    },
+
+    getTaskList: () => {
+      return { ..._taskList };
+    },
+  };
+
+  return publicMethods;
 }
