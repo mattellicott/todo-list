fd2253f7a401885a0509a9fdb73b9551e10a1ea2
Matt Ellicott <mattellicott@gmail.com>
2023-11-04 18:27:35 -0500

    add: deleteProperty() privatized condenseObject()

--- src/modules/PublicFunctions.js
+++ src/modules/PublicFunctions.js
@@ -1,8 +1,24 @@
-// Condense after Key/Value deleted
-export function condenseObject(obj, startKey, endKey) {
-  for (let i = startKey; i < endKey; i++) {
-    obj[i] = obj[+i + 1];
+export function deleteProperty(obj, property) {
+  let propDeleted = false;
+  let endKey = Object.keys(obj).length - 1;
+
+  for (const key in obj) {
+    if (obj[key].getTask() === property) {
+      propDeleted = true;
+      delete obj.key;
+    }
+
+    if (propDeleted) {
+      condenseObject(obj, key, endKey);
+      break;
+    }
   }
 
-  delete obj[endKey];
+  function condenseObject(obj, startKey, endKey) {
+    for (let i = startKey; i < endKey; i++) {
+      obj[i] = obj[+i + 1];
+    }
+
+    delete obj[endKey];
+  }
 }
