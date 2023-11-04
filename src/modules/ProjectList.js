a3c3e5dfd76e0de3ead98298484c173c9e5e3052
Matt Ellicott <mattellicott@gmail.com>
2023-11-04 18:33:15 -0500

    add: publicMethods addProject, deleteProject, getProjectTitles

--- src/modules/ProjectList.js
+++ src/modules/ProjectList.js
@@ -1,9 +1,29 @@
+import { deleteProperty } from "./PublicFunctions";
+
 export const projectList = function () {
   const _projectList = {};
 
   const publicMethods = {
+    addProject: (project) => {
+      _projectList[Object.keys(_projectList).length] = project;
+    },
+
+    deleteProject: (project) => {
+      deleteProperty(_projectList, project);
+    },
+
     getProjects: () => {
       return _projectList;
+    },
+
+    getProjectTitles: () => {
+      const titles = [];
+
+      for (const key in _projectList) {
+        titles.push(_projectList[key].getTitle());
+      }
+
+      return titles;
     },
   };
 
