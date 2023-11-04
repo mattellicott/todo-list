4be4d2d1d0be31af0286a4ba6492ab1c6efa1fb7
Matt Ellicott <mattellicott@gmail.com>
2023-11-04 17:38:25 -0500

    add: publicMethod deleteTask

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -7,6 +7,23 @@ function Project(title) {
       _taskList[Object.keys(_taskList).length] = task;
     },
 
+    deleteTask: (task) => {
+      let taskDeleted = false;
+      let endKey = Object.keys(_taskList).length - 1;
+
+      for (const key in _taskList) {
+        if (_taskList[key].getTask() === task) {
+          taskDeleted = true;
+          delete _taskList.key;
+        }
+
+        if (taskDeleted) {
+          condenseTaskList(key, endKey);
+          break;
+        }
+      }
+    },
+
     getTitle: () => {
       return _title;
     },
