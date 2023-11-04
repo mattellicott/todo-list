e820fc6a5537a1ad5ea4eadc686c3ee5a6ca82df
Matt Ellicott <mattellicott@gmail.com>
2023-11-04 16:30:08 -0500

    add: publicMethods getter: getTaskClone setters: setTitle, setDescription, setDueDate, setPriority, setCompleted

--- src/modules/Task.js
+++ src/modules/Task.js
@@ -1,3 +1,27 @@
 export function Task(title, description, dueDate, priority = 0, completed = false) {
-  const task = { title, description, dueDate, priority, completed };
+  const _task = { title, description, dueDate, priority, completed };
+
+  const publicMethods = {
+    getTaskClone: () => {
+      return { ..._task };
+    },
+
+    setTitle: (title) => {
+      _task.title = title;
+    },
+    setDescription: (description) => {
+      _task.description = description;
+    },
+    setDueDate: (dueDate) => {
+      _task.dueDate = dueDate;
+    },
+    setPriority: (priority) => {
+      _task.priority = priority;
+    },
+    setCompleted: (completed) => {
+      _task.completed = completed;
+    },
+  };
+
+  return publicMethods;
 }
