280a445931855aaf0a95a8a1627d1c78e172030b
Matt Ellicott <mattellicott@gmail.com>
2023-11-05 14:15:55 -0600

    update: DEFAULT_ELEMENT_IDS

--- src/modules/data-initialization.js
+++ src/modules/data-initialization.js
@@ -1,13 +1,13 @@
 import { ProjectList } from "./project-list";
 import { UserInterface } from "./user-interface";
 
-const DEFAULT_ELEMENTS = {
-  sidebar: document.getElementById("sidebar"),
-  main: document.getElementById("main"),
+const DEFAULT_ELEMENT_IDS = {
+  sidebar: "sidebar",
+  main: "main",
 };
 const DEFAULT_TAB_NAMES = ["All", "Today", "Week", "Important", "Completed"];
 
 const projectList = ProjectList();
 const ui = UserInterface();
 
-export { DEFAULT_TAB_NAMES, DEFAULT_ELEMENTS, projectList, ui };
+export { DEFAULT_TAB_NAMES, DEFAULT_ELEMENT_IDS, projectList, ui };
