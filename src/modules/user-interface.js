f2f490a78280f95bf4fc70ffeb3bcf78bb02d561
Matt Ellicott <mattellicott@gmail.com>
2023-11-05 13:48:14 -0600

    rename: UI to UserInterface

--- src/modules/user-interface.js
+++ src/modules/user-interface.js
@@ -2,6 +2,4 @@ import { projectList } from "./project-list";
 import { Project } from "./project";
 import { Task } from "./task";
 
-export const UI = {
-  init: () => {},
-};
+export function UserInterface() {}
