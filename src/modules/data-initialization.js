97a7f4f281c859eb63723ae7a51d7739bc21245a
Matt Ellicott <mattellicott@gmail.com>
2023-11-05 14:04:54 -0600

    add: variables

---
+++ src/modules/data-initialization.js
@@ -0,0 +1,13 @@
+import { ProjectList } from "./project-list";
+import { UserInterface } from "./user-interface";
+
+const DEFAULT_TAB_NAMES = ["All", "Today", "Week", "Important", "Completed"];
+const DEFAULT_ELEMENTS = {
+  sidebar: document.getElementById("sidebar"),
+  main: document.getElementById("main"),
+};
+
+const projects = ProjectList();
+const ui = UserInterface();
+
+export { DEFAULT_TAB_NAMES, DEFAULT_ELEMENTS, projects, ui };
