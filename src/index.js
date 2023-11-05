013f6bd54260e8b5e51ad35f5757ea2751a478a8
Matt Ellicott <mattellicott@gmail.com>
2023-11-05 13:51:04 -0600

    remove: unnecessary imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import "./style.css";
-import { UserInterface } from "./modules/user-interface";
-import { ProjectList } from "./modules/project-list";
-
-const projects = ProjectList();
-const UI = UserInterface();
 
 UI.init();
-
-export { projects };
