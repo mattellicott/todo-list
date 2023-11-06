baeb35f9a15aff72b31b470b3b2c1a8a92b6fcb8
Matt Ellicott <mattellicott@gmail.com>
2023-11-06 16:38:03 -0600

    add: newProjectHandler

--- src/modules/user-interface.js
+++ src/modules/user-interface.js
@@ -2,14 +2,14 @@ import { Project } from "./project";
 import { Task } from "./task";
 import { projectList, filterList } from "..";
 
-const projectTabsElement = document.getElementById("sidebar-project-tabs");
-
 const sidebar = Sidebar();
+
+const projectTabsElement = document.getElementById("sidebar-project-tabs");
 
 export function UserInterface() {
   const publicMethods = {
     load: () => {
-      sidebar.addTabs();
+      sidebar.loadTabs();
     },
   };
 
@@ -17,27 +17,70 @@ export function UserInterface() {
 }
 
 function Sidebar() {
-  const publicMethods = {
-    addTabs: () => {
-      addProjectTabs();
+  return {
+    loadTabs: () => {
+      loadProjectTabs();
     },
   };
-
-  return publicMethods;
 }
 
-function addProjectTabs() {
+function loadProjectTabs() {
   for (const project of projectList.getProjects()) {
+    const projectTitle = project.getTitle();
     const buttonElement = document.createElement("button");
 
-    buttonElement.innerHTML = project.getTitle();
-    buttonElement.id = project.getTitle() + "-tab";
+    buttonElement.innerHTML = projectTitle;
+    buttonElement.id = projectTitle + "-tab";
+    buttonElement.title = projectTitle;
     buttonElement.addEventListener("click", () => clickSidebarTab(project));
 
     projectTabsElement.appendChild(buttonElement);
   }
 }
 
+function reloadProjectTabs() {
+  while (projectTabsElement.firstChild) {
+    projectTabsElement.removeChild(projectTabsElement.firstChild);
+  }
+
+  loadProjectTabs();
+}
+
 function clickSidebarTab(project) {
   console.log("CLICKING ME SHOULD LOAD PROJECT " + project.getTitle() + " INTO MAIN");
 }
+
+(function NewProjectHandler() {
+  const newProjectBtn = document.getElementById("sidebar-project-new-button");
+  const newProjectDialog = document.getElementById("new-project-dialog");
+  const newProjectForm = document.getElementById("new-project-form");
+  const newProjectFormTitle = document.getElementById("new-project-form-title");
+  const newProjectFormCancel = document.getElementById("new-project-form-cancel");
+  const newProjectFormCreate = document.getElementById("new-project-form-create");
+
+  newProjectBtn.addEventListener("click", clickNewProject);
+  newProjectFormCreate.addEventListener("click", submitNewProjectForm);
+  newProjectFormCancel.addEventListener("click", closeNewProjectDialog);
+  newProjectDialog.addEventListener("close", closeNewProjectDialog);
+
+  function clickNewProject() {
+    newProjectDialog.showModal();
+  }
+
+  function submitNewProjectForm() {
+    const titleValue = newProjectFormTitle.value;
+
+    if (titleValue) {
+      projectList.addProject(Project(titleValue));
+      reloadProjectTabs();
+    }
+  }
+
+  function closeNewProjectDialog() {
+    if (newProjectDialog.hasAttribute("open")) {
+      newProjectDialog.close();
+    }
+
+    newProjectForm.reset();
+  }
+})();
