221d001156e03fc354be674883ad3cee5a849343
Matt Ellicott <mattellicott@gmail.com>
2023-11-06 14:40:45 -0600

    add: imports

--- src/modules/user-interface.js
+++ src/modules/user-interface.js
@@ -1,6 +1,6 @@
-import { DEFAULT_ELEMENT_IDS, DEFAULT_TAB_NAMES, projectList } from "./data-initialization";
 import { Project } from "./project";
 import { Task } from "./task";
+import { projectList, filterList } from "..";
 
 export function UserInterface() {
   const publicMethods = {
