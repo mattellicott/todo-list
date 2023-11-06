b1559fa42b360ee47c8b51e5d91e0b22b56ea433
Matt Ellicott <mattellicott@gmail.com>
2023-11-06 13:51:52 -0600

    rename: variable filters to filterList

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import { Filters } from "./modules/filters";
 import { ProjectList } from "./modules/project-list";
 import { UserInterface } from "./modules/user-interface";
 
-const filters = Filters();
+const filterList = Filters();
 const projectList = ProjectList();
 const ui = UserInterface();
 
 ui.load();
 
-export { projectList, filters };
+export { projectList, filterList };
