a5527448a27c1b104d8fad08fb320c7101c7f2e3
Matt Ellicott <mattellicott@gmail.com>
2023-11-08 15:15:11 -0600

    add: newTaskHandler

--- src/modules/user-interface.js
+++ src/modules/user-interface.js
@@ -10,6 +10,12 @@ const projectTabsElement = document.getElementById("sidebar-project-tabs");
 export function UserInterface() {
   const publicMethods = {
     load: () => {
+      const proj = Project("Chores");
+      projectList.addProject(proj);
+      proj.addTask(Task("Car1", "Wash the car", "11/5/23", 1, false));
+      proj.addTask(Task("Car2", "Wash the car", "11/5/23", 1, true));
+      proj.addTask(Task("Car3", "Wash the car", "11/5/23", 1, false));
+
       sidebar.loadTabs();
     },
   };
@@ -19,15 +25,20 @@ export function UserInterface() {
 
 function loadProjectPage(project) {
   const pageHeader = document.getElementById("project-page-header");
-  const pageAddTaskBtn = document.getElementById("project-page-add-task-button");
-  const addTaskForm = document.getElementById("add-task-form");
+  const pageNewTaskBtn = document.getElementById(
+    "project-page-new-task-button",
+  );
   const pageTasks = document.getElementById("project-page-tasks");
 
-  pageAddTaskBtn.addEventListener("click", clickAddTaskBtn);
+  pageNewTaskBtn.addEventListener("click", newTaskHandler);
 
   resetPage();
-  loadHeader(project.getTitle());
-  loadTasks(project.getTasks());
+  loadPage();
+
+  function loadPage() {
+    loadHeader(project.getTitle());
+    loadTasks(project.getTasks());
+  }
 
   function resetPage() {
     removeElementChildren(pageHeader);
@@ -42,19 +53,48 @@ function loadProjectPage(project) {
     for (const key in taskList) {
       const task = taskList[key].getTask();
 
-      pageTasks.appendChild(TaskElement(task));
+      pageTasks.appendChild(createTaskElement(task));
     }
   }
 
-  function clickAddTaskBtn() {
-    showAddTaskForm();
+  function newTaskHandler() {
+    const form = document.getElementById("new-task-form");
+    const formTitle = document.getElementById("new-task-form-title");
+    const formDescription = document.getElementById("new-task-form-desc");
+    const formDueDate = document.getElementById("new-task-form-duedate");
+    const formPriority = document.getElementById("new-task-form-priority");
+    const formCompleted = document.getElementById("new-task-form-completed");
+    const formCancelBtn = document.getElementById("new-task-form-cancel");
+
+    formCancelBtn.addEventListener("click", resetTaskHandler);
+    form.addEventListener("submit", submitForm);
+
+    form.style.display = "block";
+    pageNewTaskBtn.style.display = "none";
+
+    function resetTaskHandler() {
+      form.style.display = "none";
+      pageNewTaskBtn.style.display = "inline";
+      form.reset();
+    }
+
+    function submitForm() {
+      project.addTask(
+        Task(
+          formTitle.value,
+          formDescription.value,
+          formDueDate.value,
+          formPriority.value,
+          formCompleted.value,
+        ),
+      );
+      resetTaskHandler();
+      resetPage();
+      loadPage();
+    }
   }
 
-  function showAddTaskForm() {
-    addTaskForm.style.visibility = "visible";
-  }
-
-  function TaskElement(task) {
+  function createTaskElement(task) {
     const wrapperElement = document.createElement("div");
     wrapperElement.classList.add("task");
 
@@ -63,15 +103,18 @@ function loadProjectPage(project) {
     wrapperElement.appendChild(createDueDate());
     wrapperElement.appendChild(createPriority());
     wrapperElement.appendChild(createCompleted());
+    wrapperElement.appendChild(createDeleteTask());
 
     return wrapperElement;
 
     function createTitle() {
-      const element = document.createElement("div");
+      let element = document.createElement("div");
       element.classList.add("task-title");
       element.innerHTML = task.title;
 
-      element.addEventListener("click", () => console.log("NEED CODE: Click me and I should be editable"));
+      element.addEventListener("click", () =>
+        console.log("NEED CODE: Click me and I should be editable"),
+      );
 
       return element;
     }
@@ -81,7 +124,9 @@ function loadProjectPage(project) {
       element.classList.add("task-description");
       element.innerHTML = task.description;
 
-      element.addEventListener("click", () => console.log("NEED CODE: Click me and I should be editable"));
+      element.addEventListener("click", () =>
+        console.log("NEED CODE: Click me and I should be editable"),
+      );
 
       return element;
     }
@@ -91,27 +136,54 @@ function loadProjectPage(project) {
       element.classList.add("task-duedate");
       element.innerHTML = task.dueDate;
 
-      element.addEventListener("click", () => console.log("NEED CODE: Click me and I should be editable"));
+      element.addEventListener("click", () =>
+        console.log("NEED CODE: Click me and I should be editable"),
+      );
 
       return element;
     }
 
     function createPriority() {
-      const element = document.createElement("div");
+      const element = document.createElement("input");
       element.classList.add("task-priority");
-      element.innerHTML = task.priority;
+      element.type = "number";
+      element.setAttribute("min", "0");
+      element.setAttribute("max", "10");
+      element.value = task.priority;
 
-      element.addEventListener("click", () => console.log("NEED CODE: Click me and I should be editable"));
+      element.addEventListener("change", () => {
+        element.value <= 10 && element.value >= 0
+          ? (task.priority = element.value)
+          : (element.value = task.priority);
+      });
 
       return element;
     }
 
     function createCompleted() {
-      const element = document.createElement("div");
+      const element = document.createElement("input");
       element.classList.add("task-completed");
-      element.innerHTML = task.completed;
+      element.type = "checkbox";
+      element.checked = task.completed;
 
-      element.addEventListener("click", () => console.log("NEED CODE: Click me and I should be editable"));
+      element.addEventListener("change", () => {
+        task.completed = element.checked;
+      });
+
+      return element;
+    }
+
+    function createDeleteTask() {
+      const element = document.createElement("button");
+      element.classList.add("task-delete-button");
+      element.innerHTML = "×";
+
+      element.addEventListener("click", () => {
+        if (confirm("Are you sure you wish to remove this task?")) {
+          pageTasks.removeChild(wrapperElement);
+          project.deleteTask(task);
+        }
+      });
 
       return element;
     }
@@ -173,19 +245,25 @@ function removeElementChildren(element) {
   const newProjectDialog = document.getElementById("new-project-dialog");
   const newProjectForm = document.getElementById("new-project-form");
   const newProjectFormTitle = document.getElementById("new-project-form-title");
-  const newProjectFormCancel = document.getElementById("new-project-form-cancel");
+  const newProjectFormCancel = document.getElementById(
+    "new-project-form-cancel",
+  );
 
   newProjectBtn.addEventListener("click", clickNewProject);
   newProjectForm.addEventListener("submit", submitNewProjectForm);
   newProjectFormCancel.addEventListener("click", closeNewProjectDialog);
   newProjectDialog.addEventListener("close", closeNewProjectDialog);
 
+  let newProject = {};
+
   function clickNewProject() {
     newProjectDialog.showModal();
   }
 
   function submitNewProjectForm() {
-    projectList.addProject(Project(newProjectFormTitle.value));
+    newProject = Project(newProjectFormTitle.value);
+
+    projectList.addProject(newProject);
     sidebar.reloadProjectTabs();
   }
 
@@ -195,5 +273,7 @@ function removeElementChildren(element) {
     }
 
     newProjectForm.reset();
+
+    loadProjectPage(newProject);
   }
 })();
