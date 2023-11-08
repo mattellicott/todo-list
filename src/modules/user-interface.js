1c7dfff6608c8888a2c2e1e3657f357d2557a241
Matt Ellicott <mattellicott@gmail.com>
2023-11-08 12:29:34 -0600

    add: loadProjectPage

--- src/modules/user-interface.js
+++ src/modules/user-interface.js
@@ -16,12 +16,120 @@ export function UserInterface() {
   return publicMethods;
 }
 
+function loadProjectPage(project) {
+  const pageHeader = document.getElementById("project-page-header");
+  const pageAddTaskBtn = document.getElementById("project-page-add-task-button");
+  const addTaskForm = document.getElementById("add-task-form");
+  const pageTasks = document.getElementById("project-page-tasks");
+
+  pageAddTaskBtn.addEventListener("click", clickAddTaskBtn);
+
+  resetPage();
+  loadHeader(project.getTitle());
+  loadTasks(project.getTasks());
+
+  function resetPage() {
+    removeElementChildren(pageHeader);
+    removeElementChildren(pageTasks);
+  }
+
+  function loadHeader(projectTitle) {
+    pageHeader.innerHTML = projectTitle;
+  }
+
+  function loadTasks(taskList) {
+    for (const key in taskList) {
+      const task = taskList[key].getTask();
+
+      pageTasks.appendChild(TaskElement(task));
+    }
+  }
+
+  function clickAddTaskBtn() {
+    showAddTaskForm();
+  }
+
+  function showAddTaskForm() {
+    addTaskForm.style.visibility = "visible";
+  }
+
+  function TaskElement(task) {
+    const wrapperElement = document.createElement("div");
+    wrapperElement.classList.add("task");
+
+    wrapperElement.appendChild(createTitle());
+    wrapperElement.appendChild(createDescription());
+    wrapperElement.appendChild(createDueDate());
+    wrapperElement.appendChild(createPriority());
+    wrapperElement.appendChild(createCompleted());
+
+    return wrapperElement;
+
+    function createTitle() {
+      const element = document.createElement("div");
+      element.classList.add("task-title");
+      element.innerHTML = task.title;
+
+      element.addEventListener("click", () => console.log("NEED CODE: Click me and I should be editable"));
+
+      return element;
+    }
+
+    function createDescription() {
+      const element = document.createElement("div");
+      element.classList.add("task-description");
+      element.innerHTML = task.description;
+
+      element.addEventListener("click", () => console.log("NEED CODE: Click me and I should be editable"));
+
+      return element;
+    }
+
+    function createDueDate() {
+      const element = document.createElement("div");
+      element.classList.add("task-duedate");
+      element.innerHTML = task.dueDate;
+
+      element.addEventListener("click", () => console.log("NEED CODE: Click me and I should be editable"));
+
+      return element;
+    }
+
+    function createPriority() {
+      const element = document.createElement("div");
+      element.classList.add("task-priority");
+      element.innerHTML = task.priority;
+
+      element.addEventListener("click", () => console.log("NEED CODE: Click me and I should be editable"));
+
+      return element;
+    }
+
+    function createCompleted() {
+      const element = document.createElement("div");
+      element.classList.add("task-completed");
+      element.innerHTML = task.completed;
+
+      element.addEventListener("click", () => console.log("NEED CODE: Click me and I should be editable"));
+
+      return element;
+    }
+  }
+}
+
 function Sidebar() {
-  return {
+  const publicMethods = {
     loadTabs: () => {
       loadProjectTabs();
     },
+
+    reloadProjectTabs: () => {
+      removeElementChildren(projectTabsElement);
+      loadProjectTabs();
+    },
   };
+
+  return publicMethods;
 }
 
 function loadProjectTabs() {
@@ -32,22 +140,31 @@ function loadProjectTabs() {
     buttonElement.innerHTML = projectTitle;
     buttonElement.id = projectTitle + "-tab";
     buttonElement.title = projectTitle;
-    buttonElement.addEventListener("click", () => clickSidebarTab(project));
+    buttonElement.addEventListener("click", (e) => {
+      updateActiveClassTabs(e);
+      clickSidebarTab(project);
+    });
 
     projectTabsElement.appendChild(buttonElement);
   }
 }
 
-function reloadProjectTabs() {
-  while (projectTabsElement.firstChild) {
-    projectTabsElement.removeChild(projectTabsElement.firstChild);
+function updateActiveClassTabs(event) {
+  for (const child of filterTabsElement.children) {
+    child.classList.remove("active");
   }
-
-  loadProjectTabs();
+  for (const child of projectTabsElement.children) {
+    child.classList.remove("active");
+  }
+  event.target.classList.add("active");
 }
 
 function clickSidebarTab(project) {
-  console.log("CLICKING ME SHOULD LOAD PROJECT " + project.getTitle() + " INTO MAIN");
+  loadProjectPage(project);
+}
+
+function removeElementChildren(element) {
+  element.innerHTML = "";
 }
 
 (function NewProjectHandler() {
@@ -68,7 +185,7 @@ function clickSidebarTab(project) {
 
   function submitNewProjectForm() {
     projectList.addProject(Project(newProjectFormTitle.value));
-    reloadProjectTabs();
+    sidebar.reloadProjectTabs();
   }
 
   function closeNewProjectDialog() {
