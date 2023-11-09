4dcb0cd1fdbed73a73822a165e181e2c2b062fa8
Matt Ellicott <mattellicott@gmail.com>
2023-11-09 14:39:30 -0600

    add: ProjectPage

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import "./style.css";
 import { Filters } from "./modules/filters";
 import { ProjectList } from "./modules/data/project-list";
 import { Sidebar } from "./modules/ui/sidebar";
+import { ProjectPage } from "./modules/ui/project-page,js";
 
 const filterList = Filters();
 const projectList = ProjectList();
 const sidebar = Sidebar();
+const projectPage = ProjectPage();
 
 sidebar.load();
 
-export { projectList, filterList };
+export { projectList, filterList, projectPage };
