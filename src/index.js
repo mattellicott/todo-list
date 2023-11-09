ebb86a92319d859cc6dabbfa6acccd632c91aaca
Matt Ellicott <mattellicott@gmail.com>
2023-11-09 15:29:12 -0600

    add: DEFAULT_PROJECT

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { Filters } from "./modules/filters";
 import { ProjectList } from "./modules/data/project-list";
 import { Sidebar } from "./modules/ui/sidebar";
 import { ProjectPage } from "./modules/ui/project-page,js";
+import { Project } from "./modules/data/project";
+
+const DEFAULT_PROJECT = Project();
 
 const filterList = Filters();
 const projectList = ProjectList();
@@ -10,5 +13,6 @@ const sidebar = Sidebar();
 const projectPage = ProjectPage();
 
 sidebar.load();
+projectPage.load(DEFAULT_PROJECT);
 
 export { projectList, filterList, projectPage };
