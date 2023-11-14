ea23321a308540590b6dbdfd0deb5e0429e22b3a
Matt Ellicott <mattellicott@gmail.com>
2023-11-14 14:37:39 -0600

    add: project parameter add: getProject

--- src/modules/data-handling/task.js
+++ src/modules/data-handling/task.js
@@ -1,7 +1,11 @@
-export function Task(title, description, dueDate, priority, completed) {
-  const _task = { title, description, dueDate, priority, completed };
+export function Task(project, title, description, dueDate, priority, completed) {
+  const _task = { project, title, description, dueDate, priority, completed };
 
   const publicMethods = {
+    getProject: () => {
+      return _task.project;
+    },
+
     getTitle: () => {
       return _task.title;
     },
@@ -25,15 +29,19 @@ export function Task(title, description, dueDate, priority, completed) {
     setTitle: (title) => {
       _task.title = title;
     },
+
     setDescription: (description) => {
       _task.description = description;
     },
+
     setDueDate: (dueDate) => {
       _task.dueDate = dueDate;
     },
+
     setPriority: (priority) => {
       _task.priority = priority;
     },
+
     setCompleted: (completed) => {
       _task.completed = completed;
     },
