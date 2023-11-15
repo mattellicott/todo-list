76cacb67e16b2ada2f57c31509dcde1125a7ed12
Matt Ellicott <mattellicott@gmail.com>
2023-11-14 19:46:16 -0600

    remove: unneccesary declarations

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import "./style.css";
-import { Storage } from "./modules/data-handlers/storage";
-import { Filters } from "./modules/data-handlers/filters";
-import { ProjectList } from "./modules/data-handlers/project-list";
 import { Sidebar } from "./modules/ui/sidebar";
-import { ProjectPage } from "./modules/ui/project-page,js";
-
-const filterList = Filters();
-const projectList = ProjectList();
-const sidebar = Sidebar();
-const projectPage = ProjectPage();
+import { Storage } from "./modules/data-handlers/storage";
 
 Storage.loadProjects();
-
-sidebar.load();
-projectPage.load(projectList.getActive());
-
-export { projectList, filterList, projectPage };
+Sidebar.load();
