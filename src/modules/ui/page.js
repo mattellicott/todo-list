41577755466d870f5c8aeb5e2ef23db99133af01
Matt Ellicott <mattellicott@gmail.com>
2023-11-16 17:51:54 -0600

    add: tasklistSortHandler

--- src/modules/ui/page.js
+++ src/modules/ui/page.js
@@ -1,5 +1,6 @@
 import { ProjectList } from "../data-handlers/project-list";
 import { Storage } from "../data-handlers/storage";
+import { compareAsc } from "date-fns";
 
 export const Page = (function () {
   const headerDiv = document.getElementById("page-header");
@@ -7,22 +8,33 @@ export const Page = (function () {
   const defaultTaskMsgDiv = document.getElementById("default-task-message");
   const newTaskBtn = document.getElementById("new-task-btn");
   const newTaskForm = document.getElementById("new-task-form");
+  const tasksHeaderDiv = document.getElementById("tasks-header");
+
+  for (const child of tasksHeaderDiv.children)
+    child.addEventListener("click", (e) => tasklistSortHandler(e));
 
   const observer = new MutationObserver(setDefaultTaskMsg);
   observer.observe(tasklistDiv, { childList: true });
 
   let _pageType;
-  let _taskContainer;
+  let _sortType;
+  let _lastSortType;
+  let _isRepeatedSortType;
+  let _title;
+  let _tasks;
 
   const publicMethods = {
     load: (taskContainer, type) => {
-      _taskContainer = taskContainer;
+      _title = taskContainer.getTitle();
+      _tasks = Object.values(taskContainer.getTasks());
       _pageType = type;
+      _isRepeatedSortType = false;
+      _lastSortType = _pageType == 'filter' ? 'project-title' : 'title';
 
       reset();
       setDefaultTaskMsg();
-      loadHeader(_taskContainer.getTitle());
-      loadTasklist(_taskContainer.getTasks());
+      loadHeader();
+      loadTasklist(_tasks);
     },
 
     addTaskElement: (task) => {
@@ -37,7 +49,7 @@ export const Page = (function () {
         : "";
 
     const noTaskMsg =
-      Object.keys(_taskContainer.getTasks()).length == 0
+      _tasks.length == 0
         ? `Create ${noProjMsg}a New Task to get started!`
         : "";
 
@@ -46,20 +58,109 @@ export const Page = (function () {
 
   function reset() {
     headerDiv.innerHTML = "";
-    while (tasklistDiv.childElementCount > 1)
-      tasklistDiv.removeChild(tasklistDiv.lastChild);
+    resetTasklist();
 
     newTaskForm.style.display = "none";
     newTaskBtn.style.display = "none";
   }
 
-  function loadHeader(title) {
-    headerDiv.innerHTML = title + " Tasks";
+  function loadHeader() {
+    headerDiv.innerHTML = _title + " Tasks";
   }
 
   function loadTasklist(tasks) {
-    for (const key in tasks) {
-      tasklistDiv.appendChild(createTaskElement(tasks[key]));
+    tasksHeaderDiv.firstElementChild.style.display =
+      _pageType == "filter" ? "initial" : "none";
+
+    for (const task of tasks) {
+      tasklistDiv.appendChild(createTaskElement(task));
+    }
+  }
+
+  function resetTasklist() {
+    while (tasklistDiv.childElementCount > 1)
+    tasklistDiv.removeChild(tasklistDiv.lastChild);
+  }
+
+  function reloadTasklist(tasks) {
+    resetTasklist();
+    loadTasklist(tasks);
+  }
+
+  function tasklistSortHandler(event) {
+    _sortType = event.target.className;
+
+    if (_sortType == _lastSortType) _isRepeatedSortType = !_isRepeatedSortType;
+    else _isRepeatedSortType = false;
+
+    sortTasklist();
+    _lastSortType = _sortType;
+
+    function sortTasklist() {
+      const _tasksCopy = [..._tasks];
+
+      switch (_sortType) {
+        case "project-title":
+          if (!_isRepeatedSortType)
+            _tasksCopy.sort((a, b) =>
+              sortString(a.getProject().getTitle(), b.getProject().getTitle()),
+            );
+          else
+            _tasksCopy.sort((b, a) =>
+              sortString(a.getProject().getTitle(), b.getProject().getTitle()),
+            );
+          break;
+        case "title":
+          if (!_isRepeatedSortType)
+            _tasksCopy.sort((a, b) => sortString(a.getTitle(), b.getTitle()));
+          else
+            _tasksCopy.sort((b, a) => sortString(a.getTitle(), b.getTitle()));
+          break;
+        case "description":
+          if (!_isRepeatedSortType)
+            _tasksCopy.sort((a, b) =>
+              sortString(a.getDescription(), b.getDescription()),
+            );
+          else
+            _tasksCopy.sort((b, a) =>
+              sortString(a.getDescription(), b.getDescription()),
+            );
+          break;
+        case "due-date":
+          if (!_isRepeatedSortType)
+            _tasksCopy.sort((a, b) => sortDate(a.getDueDate(), b.getDueDate()));
+          else
+            _tasksCopy.sort((b, a) => sortDate(a.getDueDate(), b.getDueDate()));
+          break;
+        case "priority":
+          if (!_isRepeatedSortType)
+            _tasksCopy.sort((a, b) => a.getPriority() - b.getPriority());
+          else _tasksCopy.sort((b, a) => a.getPriority() - b.getPriority());
+          break;
+        case "completed":
+          if (!_isRepeatedSortType)
+            _tasksCopy.sort((a, b) => a.getCompleted() - b.getCompleted());
+          else _tasksCopy.sort((b, a) => a.getCompleted() - b.getCompleted());
+          break;
+      }
+
+      reloadTasklist(_tasksCopy);
+
+      function sortString(aValue, bValue) {
+        return aValue.localeCompare(bValue, undefined, {
+          numeric: true,
+        });
+      }
+
+      function sortDate(aValue, bValue) {
+        const [aMonth, aDay, aYear] = aValue.split("/");
+        const [bMonth, bDay, bYear] = bValue.split("/");
+
+        return compareAsc(
+          new Date(aYear, aMonth - 1, aDay),
+          new Date(bYear, bMonth - 1, bDay),
+        );
+      }
     }
   }
 
@@ -83,60 +184,78 @@ export const Page = (function () {
 
     function createProjectTitle() {
       const title = task.getProject().getTitle();
+      const elementWrapper = document.createElement("div");
       const element = document.createElement("div");
 
-      element.classList.add("project-title");
+      elementWrapper.classList.add("project-title");
+      elementWrapper.appendChild(element);
+
       element.innerHTML = title;
 
-      return element;
+      return elementWrapper;
     }
 
     function createTaskTitle() {
       const title = task.getTitle();
+      const elementWrapper = document.createElement("div");
       const element = document.createElement("div");
 
-      element.classList.add("title");
+      elementWrapper.classList.add("title");
+      elementWrapper.appendChild(element);
+
       element.innerHTML = title;
 
-      return element;
+      return elementWrapper;
     }
 
     function createDescription() {
       const description = task.getDescription();
+      const elementWrapper = document.createElement("div");
       const element = document.createElement("div");
 
-      element.classList.add("description");
+      elementWrapper.classList.add("description");
+      elementWrapper.appendChild(element);
+
       element.innerHTML = description;
 
-      return element;
+      return elementWrapper;
     }
 
     function createDueDate() {
       const dueDate = task.getDueDate();
+      const elementWrapper = document.createElement("div");
       const element = document.createElement("div");
 
-      element.classList.add("due-date");
+      elementWrapper.classList.add("due-date");
+      elementWrapper.appendChild(element);
+
       element.innerHTML = dueDate;
 
-      return element;
+      return elementWrapper;
     }
 
     function createPriority() {
       const priority = task.getPriority();
+      const elementWrapper = document.createElement("div");
       const element = document.createElement("div");
 
-      element.classList.add("priority");
+      elementWrapper.classList.add("priority");
+      elementWrapper.appendChild(element);
+
       element.classList.add(["low", "medium", "high"][priority]);
       element.innerHTML = ["Low", "Medium", "High"][priority];
 
-      return element;
+      return elementWrapper;
     }
 
     function createCompleted() {
       const completed = task.getCompleted();
+      const elementWrapper = document.createElement("div");
       const element = document.createElement("input");
 
-      element.classList.add("completed");
+      elementWrapper.classList.add("completed");
+      elementWrapper.appendChild(element);
+
       element.type = "checkbox";
       element.checked = completed ? true : false;
 
@@ -144,23 +263,27 @@ export const Page = (function () {
         task.setCompleted(element.checked),
       );
 
-      return element;
+      return elementWrapper;
     }
 
     function createDeleteTask() {
+      const elementWrapper = document.createElement("div");
       const element = document.createElement("div");
-      element.classList.add("delete-button");
+
+      elementWrapper.classList.add("delete-btn");
+      elementWrapper.appendChild(element);
+
       element.innerHTML = "×";
 
       element.addEventListener("click", (e) => {
         if (confirm("Are you sure you wish to remove this task?")) {
-          e.target.parentElement.remove();
+          e.target.parentElement.parentElement.remove();
           task.getProject().deleteTask(task);
           Storage.saveProjects();
         }
       });
 
-      return element;
+      return elementWrapper;
     }
   }
 
